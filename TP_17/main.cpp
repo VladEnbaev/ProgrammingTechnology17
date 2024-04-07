ceb374ededef17b09554c90d1d06a6ba98ce6210
VladGradskiy <devvlad.gradskiy@gmail.com>
2024-04-07 16:13:56 +0300

    Initial Commit

---
+++ TP_17/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TP_17
+//
+//  Created by Vlad Gradskiy on 07.04.2024.
+//  
+
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
