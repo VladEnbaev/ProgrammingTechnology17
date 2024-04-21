af131748c7ea6e965b6e03758d566b00f7c6b7f7
VladGradskiy <devvlad.gradskiy@gmail.com>
2024-04-21 23:53:40 +0300

    created main task

---
+++ TP_17/Point/Point.hpp
@@ -0,0 +1,14 @@
+//
+//  Point.hpp
+//  TP_17
+//
+//  Created by Vlad Gradskiy on 07.04.2024.
+//  
+
+
+#ifndef Point_hpp
+#define Point_hpp
+
+#include <stdio.h>
+
+#endif /* Point_hpp */
---
+++ TP_17/SortedArray/SortedArray.hpp
@@ -0,0 +1,14 @@
+//
+//  SortedArray.hpp
+//  TP_17
+//
+//  Created by Vlad Gradskiy on 07.04.2024.
+//  
+
+
+#ifndef SortedArray_hpp
+#define SortedArray_hpp
+
+#include <stdio.h>
+
+#endif /* SortedArray_hpp */
